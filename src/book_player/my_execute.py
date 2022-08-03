72028709bba31e637234e68b3a32ee9ad79944b0
Luiz Eduardo Fonseca <luizfonseca.email@gmail.com>
2022-08-02 21:32:05 -0400

    Book de variaveis com ingestao

--- src/book_player/my_execute.py
+++ src/book_player/my_execute.py
@@ -1,9 +1,9 @@
 # %%
+import datetime
 
-import sqlalchemy
-import os
 
 from sqlalchemy import create_engine
+from tqdm import tqdm
 
 from ranked_ml.settings import SQLITE_DB_URL
 
@@ -25,10 +25,29 @@ def process_date(query, engine, data):
     return True
 
 
+def get_date_range_list(date_start: str, date_end: str) -> list:
+    date_start = datetime.datetime.strptime(date_start, "%Y-%m-%d")
+    date_end = datetime.datetime.strptime(date_end, "%Y-%m-%d")
+    days = (date_end - date_start).days
+    dates = [(date_start + datetime.timedelta(days=i)).strftime("%Y-%m-%d") for i in range(days + 1)]
+    return dates
+
+
+def backfill(query, engine, dt_start, dt_stop):
+    dates = get_date_range_list(dt_start, dt_stop)
+    for d in tqdm(dates):
+        process_date(query, engine, d)
+
+
 if __name__ == '__main__':
     engine = create_engine(SQLITE_DB_URL)
-    data = '2022-02-03'  # input("entre com uma data: ")
-    query = import_query('query_partidas.sql')
-    print(query)
+    # data = '2022-02-03'  #
+    date_start = input("entre com uma data de inicio: ")
+    date_end = input("entre com uma data de fim: ")
+    print( get_date_range_list(date_start, date_end) )
 
-    process_date(query, engine, data)
+    query = import_query('query_partidas.sql')
+    backfill(query, engine, date_start, date_end)
+
+    # print(query)
+    # process_date(query, engine, data)
